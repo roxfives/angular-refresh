7a012ce2e6a052456b4dafb0d06fa52795eeb5c6
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-08-26 17:18:16 -0300

    Makes heros observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -11,17 +12,26 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent 
     implements OnInit {
   
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService
+  ) { }
 
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => {
+        this.heroes = heroes;
+      });
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log('Hero selected: ' + hero.name);
   }
 }
