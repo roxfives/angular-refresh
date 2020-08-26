777c193ade8b6286e83d0783100618ce600d0ab8
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-08-26 16:30:21 -0300

    Lists all the heroes and shows specific details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,17 @@ import { Hero } from '../hero';
 export class HeroesComponent 
     implements OnInit {
   
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-  
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
-  
+
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log('Hero selected: ' + hero.name);
   }
 }
